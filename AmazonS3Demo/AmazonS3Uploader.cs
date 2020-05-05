d57fff092417d5b782f0a37ae5c3f5a1f6e0ec4f
Harms <543232@bah.com>
2020-05-05 10:31:26 -0400

    saving a test configuration

--- AmazonS3Demo/AmazonS3Uploader.cs
+++ AmazonS3Demo/AmazonS3Uploader.cs
@@ -6,13 +6,13 @@ namespace AmazonS3Demo
 {
     public class AmazonS3Uploader
     {
-        private string bucketName = "your-amazon-s3-bucket";
-        private string keyName = "the-name-of-your-file";
-        private string filePath = "C:\\Users\\yourUserName\\Desktop\\myImageToUpload.jpg"; 
+        private string bucketName = "teststoragemanager542020";
+        private string keyName = "fileKeyName";
+        private string filePath = "C:\\Users\\543232\\Desktop\\t3upload.txt";
 
         public void UploadFile()
         {
-            var client = new AmazonS3Client(Amazon.RegionEndpoint.USEast1);
+            var client = new AmazonS3Client(Amazon.RegionEndpoint.USEast2);
 
             try
             {
